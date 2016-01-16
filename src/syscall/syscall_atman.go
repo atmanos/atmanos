47404c67b2a0eedf7249f8c0da83fd3b4efab620
Bernerd Schaefer <bj.schaefer@gmail.com>
2016-01-16 15:41:01 -0800

    Add minimal syscall implementation

---
+++ src/syscall/syscall_atman.go
@@ -0,0 +1,26 @@
+package syscall
+
+// An Errno is an unsigned number describing an error condition.
+// It implements the error interface.  The zero Errno is by convention
+// a non-error, so code to convert from Errno to error should use:
+//	err = nil
+//	if errno != 0 {
+//		err = errno
+//	}
+type Errno uintptr
+
+func (e Errno) Error() string {
+	return "errno " + itoa(int(e))
+}
+
+const EINVAL = Errno(0x1)
+
+type Timespec struct {
+	Sec  int64
+	Nsec int64
+}
+
+type Timeval struct {
+	Sec  int64
+	Usec int64
+}
