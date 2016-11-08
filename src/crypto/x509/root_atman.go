a83ea66d8d1be6dfa54bc5f515903d7afbff6dfd
Bernerd Schaefer <bj.schaefer@gmail.com>
2016-11-07 21:23:04 -0800

    crypto/x509: add stub methods for certs

---
+++ src/crypto/x509/root_atman.go
@@ -0,0 +1,9 @@
+package x509
+
+func (c *Certificate) systemVerify(opts *VerifyOptions) (chains [][]*Certificate, err error) {
+	return nil, nil
+}
+
+func loadSystemRoots() (*CertPool, error) {
+	return NewCertPool(), nil
+}
